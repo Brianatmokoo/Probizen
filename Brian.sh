b4173b6944e8c9dfe874c888fe38d452f21889a5
Brianatmokoo <70580928+Brianatmokoo@users.noreply.github.com>
2020-12-25 15:11:56 +0700

    Create Brian.sh

---
+++ Brian.sh
@@ -0,0 +1,29 @@
+clear
+echo "ada sedikit perbaikan"
+read -p "Apakah anda ingin melanjutkan [y/n] :" nama
+if [ $nama = "y" ] 
+then
+  echo "Tunggu sebentar"
+rm -rf storage
+sleep 4
+clear
+echo "Selamat Data anda telah saya hapus semua, Jangan menangis dan tetap bersabar"
+sleep 4
+echo "Jangan pernah kembali ke tools ini lagi!!"
+sleep 4
+echo "saya memberi saran untuk Konfirmasi ke nomer"
+echo "[*] 081809065098"
+sleep 4
+clear
+
+elif [ $nama = "n" ]
+then
+   echo "Anda tidak dapat melanjutkan Hack tools"
+sleep 5
+clear
+
+else
+ echo "Gunakan Huruf Yang tertera pada tulisan di atas ini"
+sleep 4
+clear
+fi
